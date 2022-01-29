4fb07abb387124f82a91bd6013cb0465370760e9
whpoland <63217407+whpoland@users.noreply.github.com>
2022-01-28 23:52:35 -0500

    Added conductivity, current density to update equations (J,M,ec,mc)

--- 1D_FDTD/main.cpp
+++ 1D_FDTD/main.cpp
@@ -6,33 +6,26 @@
 //namespace plt = matplotlibcpp;
 
 /* define mathematical constants */
-#define C 300000000
-#define EPS_0 0.00000000000885
-#define MU_0 4*M_PI*pow(10,-7)
+#define C 3e8
+#define EPS_0 8.85e-12
+#define MU_0 (M_PI*4e-7)
 
 /* CUSTOMIZABLE VALUES */
-#define FREQ 300*pow(10,6)
-#define EPS_R 1
-#define MU_R 1
-
-/* calculate some useful values based on inputs */
-#define EPS EPS_0*EPS_R
-#define MU MU_0*MU_R
-#define VEL C/sqrt(EPS_R)
-#define LAMBDA VEL/FREQ
-#define PERIOD 1/FREQ
-#define IMP sqrt(MU/EPS)
-#define CRNT 1 // Courant Number
+#define FREQ 300e6
 
 /* define bounds of our simulation */
+#define LAMBDA C/FREQ
+
 #define DX (LAMBDA/10.)
 #define DT (DX/C)
-#define X_MAX 100
-#define Y_MAX X_MAX
-#define Z_MAX Y_MAX
-#define T_MAX 200
-#define N 400 // the number of elements we will track at a time
+//#define X_MAX 100
+//#define Y_MAX X_MAX
+//#define Z_MAX Y_MAX
 //#define N (int)ceil(X_MAX/DX)
+
+#define T_MAX 800
+#define N 400 // the number of elements we will track at a time
+
 
 // Create a struct template for E,H fields (to contain x,y,z,cond components)
 struct field {
@@ -49,9 +42,11 @@ struct current {
     double z[N]; // z component
 };
 
-// Struct to contain important info
-struct data {
-
+struct parameters {
+    double eps[N]; // relative permittivity
+    double mu[N]; // relative permeability
+    double ec[N]; // electrical conductivity
+    double mc[N]; // magnetic conductivity
 };
 
 /* function headers */
@@ -62,22 +57,39 @@ double updateEz(int t, int delta, field* E, field *H, current* J, current* M);
 double updateHx(int t, int delta, field* E, field *H, current* J, current* M);
 double updateHy(int t, int delta, field* E, field *H, current* J, current* M);
 double updateHz(int t, int delta, field* E, field *H, current* J, current* M);
-void compute(field *E, field *H, current *J, current *M);
-
-double output[N];
+void compute(field *E, field *H, current *J, current *M, parameters *params);
 
 int main() {
-//    std::cout << "Hello, World!" << std::endl;
+//  Initialize fields
     field E = {{0.},{0.},{0.},{0.}};
     field H = {{0.},{0.},{0.},{0.}};
     current J = {{0.},{0.},{0.}};
     current M = {{0.},{0.},{0.}};
+    parameters  params = {{0.},{0.},{0.},{0.}};
+
+    for (int i = 0; i < N; i++) {
+        params.eps[i] = 1;
+        params.mu[i] = 1;
+        params.ec[i] = 0;
+        params.mc[i] = 0;
+        if (i < N/2) {
+            params.eps[i] = 10;
+        }
+    }
+
+//    params.ec[100] = 100;
+
+//    std::cout << std::endl;
+//    std::cout << "EPS0: " << EPS_0 << " MU0: " << MU_0 << " DT: " << DT << " DX: " << DX << std::endl;
+//    std::cout << "DT/DX: " << DT / DX << std::endl;
+//    std::cout << "LAMBDA: " << LAMBDA << std::endl;
+//    std::cout << "C: " << C << std::endl;
+//    std::cout << "DT/MU/DX: " << DT / MU_0 / DX << std::endl;
+//    std::cout << "DT/EPS/DX: " << DT / EPS_0 / DX << std::endl;
+
 
     // run the simulation and store in file
-    compute(&E, &H, &J, &M);
-
-    // graph the data from the file
-    //plot();
+    compute(&E, &H, &J, &M, &params);
 
     return 0;
 }
@@ -94,29 +106,35 @@ double updateHx(int t, int delta, field* E, field *H, current* J, current* M) {
 //    H->x[t - delta/2] + delta/MU*(numDeriv(E->z, t, delta) + M->x[t] + H->c[t]*H->x[t])
 }
 
-void compute(field *E, field *H, current *J, current *M) {
+void compute(field *E, field *H, current *J, current *M, parameters *params) {
     // time-stepping loop
     int t;
-    std::ofstream myfile ("/Users/williampoland/Library/Mobile Documents/com~apple~CloudDocs/Downloads/FDTDadd.txt");
+    std::string output_file = "/Users/williampoland/FDTD Output/FDTDdie.txt";
+//    std::string output_file = "/Users/williampoland/FDTD Output/FDTD3.txt";
+//    std::string output_file = "/Users/williampoland/FDTD Output/FDTDadd.txt";
+    std::ofstream myfile (output_file);
 
     for (t = 0; t < T_MAX; t++) {
         // step 1 - [TE] Hx, Hy, Ez
         int x;
-        for (x = 0; x < N - 1; x++)
-            H->y[x] += (E->z[x+1] - E->z[x]) * DT / MU / DX;
-         //never reaches Hy[N-1], so this node stays 0 -- PMC
-
+        for (x = 0; x < N - 1; x++) {
+//            H->y[x] += (E->z[x + 1] - E->z[x]) * DT / MU_0 / params->mu[x] / DX;
+//            H->y[x] += (E->z[x+1] - E->z[x]) * DT / MU_0 / params->mu[x] / DX;
+            H->y[x] += DT / MU_0 / params->mu[x] * ((E->z[x+1] - E->z[x]) / DX + H->y[x] * params->mc[x] + M->y[x]);
+            //never reaches Hy[N-1], so this node stays 0 -- PMC
+        }
         // step 2 - [TM] Ex, Ey, Hz
-        for (x = 1; x < N; x++)
-            E->z[x] += (H->y[x] - H->y[x-1]) * DT / EPS / DX;
-
+        for (x = 1; x < N; x++) {
+//            E->z[x] += (H->y[x] - H->y[x - 1]) * DT / EPS_0 / DX;
+//            E->z[x] += (H->y[x] - H->y[x-1]) * DT / EPS_0 / params->eps[x] / DX;
+            E->z[x] += DT / EPS_0 / params->eps[x] * ((H->y[x] - H->y[x-1]) / DX - E->z[x] * params->ec[x] - J->z[x]);
+        }
         /* hardwire a source node */
         // this makes Ez[0] follow a  wave shape, forcing the simulation to create a wave
-        //E->z[0] = exp(-(t - 30.) * (t - 30.) / 100.);
+//        E->z[0] = exp(-(t - 30.) * (t - 30.) / 100.);
 
         /* additive source node */
         E->z[N - 1] += exp(-(t - 30.) * (t - 30.) / 100.);
-        //    output[t] = E->z[N-1];
 
         if (myfile.is_open())
         {
@@ -129,6 +147,6 @@ void compute(field *E, field *H, current *J, current *M) {
 //        printf("%d\n",E->z[50]);
     }
     myfile.close();
-    std::cout << "Finished Writing to File" << std::endl;
+    std::cout << std::endl << "Finished Writing to File: " << output_file << std::endl;
 }
 
